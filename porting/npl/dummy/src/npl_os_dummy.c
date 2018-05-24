23d834ee035587fbe3be834d5b12325a6f549a0e
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2018-05-24 12:00:23 +0200

    porting: Add missing API to dummy NPL

--- porting/npl/dummy/src/npl_os_dummy.c
+++ porting/npl/dummy/src/npl_os_dummy.c
@@ -44,6 +44,11 @@ ble_npl_eventq_init(struct ble_npl_eventq *evq)
 
 }
 
+struct ble_npl_event *
+ble_npl_eventq_get(struct ble_npl_eventq *evq, ble_npl_time_t tmo)
+{
+}
+
 void
 ble_npl_eventq_put(struct ble_npl_eventq *evq, struct ble_npl_event *ev)
 {
