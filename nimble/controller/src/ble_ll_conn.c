e62e52087625b0893bb210d4985424d3ccdaf3c4
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2018-06-07 14:57:38 +0200

    nimble/ll: Add helper to copy from "flat" mbuf
    This helper can be used when we know that payload is stored in single
    mbuf (not chained) and starts at offset 0. This silly optimization can
    save some CPU cycles which does make difference when trying to send
    CONNECT_REQ on nRF51.

--- nimble/controller/src/ble_ll_conn.c
+++ nimble/controller/src/ble_ll_conn.c
@@ -2868,7 +2868,7 @@ ble_ll_conn_request_send(uint8_t addr_type, uint8_t *adva, uint16_t txoffset,
     } else {
         ble_phy_set_txend_cb(ble_ll_conn_req_txend_init, NULL);
     }
-    rc = ble_phy_tx(ble_ll_tx_mbuf_pducb, m, end_trans);
+    rc = ble_phy_tx(ble_ll_tx_flat_mbuf_pducb, m, end_trans);
     return rc;
 }
 
