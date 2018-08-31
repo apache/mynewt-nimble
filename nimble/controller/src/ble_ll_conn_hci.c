f9564708182071ae5b9ba6ac46250b38497a6223
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2018-08-31 11:49:05 +0200

    nimble/ll: Fix error code on connection update request
    If features has been exchanged and it is already known that peer does
    not support Connection Parameters Request procedure, controller shall
    return command status with code (0x1A) according to spec Bluetoot 5.0
    Vol 6, 5.1.7
    
    It fixes LL/CON/SLA/BV-86

--- nimble/controller/src/ble_ll_conn_hci.c
+++ nimble/controller/src/ble_ll_conn_hci.c
@@ -853,7 +853,7 @@ ble_ll_conn_hci_update(uint8_t *cmdbuf)
     /* See if this feature is supported on both sides */
     if ((connsm->conn_features & BLE_LL_FEAT_CONN_PARM_REQ) == 0) {
         if (connsm->conn_role == BLE_LL_CONN_ROLE_SLAVE) {
-            return BLE_ERR_CMD_DISALLOWED;
+            return BLE_ERR_UNSUPP_REM_FEATURE;
         }
         ctrl_proc = BLE_LL_CTRL_PROC_CONN_UPDATE;
     } else {
