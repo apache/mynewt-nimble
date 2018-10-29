680a32ce66839211138a2fa4b5e4773136ccfaea
Martin Turon <mturon@google.com>
2018-10-29 14:55:43 -0700

    [linux] Stability fixes to linux port: npl mutex, queues, and task.

--- porting/npl/linux/src/os_mutex.c
+++ porting/npl/linux/src/os_mutex.c
@@ -28,14 +28,13 @@
 ble_npl_error_t
 ble_npl_mutex_init(struct ble_npl_mutex *mu)
 {
-    pthread_mutexattr_t mu_attr;
-
     if (!mu) {
         return BLE_NPL_INVALID_PARAM;
     }
 
-    pthread_mutexattr_settype(&mu_attr, PTHREAD_MUTEX_RECURSIVE_NP);
-    pthread_mutex_init(&mu->lock, &mu_attr);
+    pthread_mutexattr_init(&mu->attr);
+    pthread_mutexattr_settype(&mu->attr, PTHREAD_MUTEX_RECURSIVE_NP);
+    pthread_mutex_init(&mu->lock, &mu->attr);
 
     return BLE_NPL_OK;
 }
@@ -57,19 +56,17 @@ ble_npl_mutex_release(struct ble_npl_mutex *mu)
 ble_npl_error_t
 ble_npl_mutex_pend(struct ble_npl_mutex *mu, uint32_t timeout)
 {
-    struct timespec wait;
-
     if (!mu) {
         return BLE_NPL_INVALID_PARAM;
     }
 
     assert(&mu->lock);
 
-    wait.tv_sec  = timeout / 1000;
-    wait.tv_nsec = (timeout % 1000) * 1000000;
-    wait.tv_nsec %= 1000000000;
+    mu->wait.tv_sec  = timeout / 1000;
+    mu->wait.tv_nsec = (timeout % 1000) * 1000000;
+    mu->wait.tv_nsec %= 1000000000;
 
-    if (pthread_mutex_timedlock(&mu->lock, &wait)) {
+    if (pthread_mutex_timedlock(&mu->lock, &mu->wait)) {
         return BLE_NPL_TIMEOUT;
     }
 
