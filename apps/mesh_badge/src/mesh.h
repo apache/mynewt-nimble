2985dc52da6db7eb92d3ecc4eaf2aaf0a3218c79
Michał Narajowski <michal.narajowski@codecoup.pl>
2018-11-28 10:32:35 +0100

    apps/mesh_badge: Port app to Mynewt

--- apps/mesh_badge/src/mesh.h
+++ apps/mesh_badge/src/mesh.h
@@ -4,9 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-void mesh_publish(void);
-void mesh_set_name(const char *name, size_t len);
+#include "mesh/mesh.h"
 
+void mesh_send_hello(void);
+
+u16_t mesh_get_addr(void);
 bool mesh_is_initialized(void);
 void mesh_start(void);
-int mesh_init(void);
+int mesh_init(u8_t addr_type);
