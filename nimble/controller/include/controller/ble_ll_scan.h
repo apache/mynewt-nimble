5ace22629d5ab0961019db4586f87b0ae2c3c3cd
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2019-01-11 10:46:06 +0100

    nimble/ll: Fix extended chaining scanning
    This patch fix scanning for the advertising events where
    last chain packet has extended header len 0

--- nimble/controller/include/controller/ble_ll_scan.h
+++ nimble/controller/include/controller/ble_ll_scan.h
@@ -98,6 +98,7 @@ struct ble_ll_scan_params
 #define BLE_LL_AUX_HAS_ADI              0x80
 
 #define BLE_LL_SET_AUX_FLAG(aux_data, flag) ((aux_data)->flags |= flag)
+#define BLE_LL_CLEAR_AUX_FLAG(aux_data, flag) ((aux_data)->flags &= ~flag)
 #define BLE_LL_CHECK_AUX_FLAG(aux_data, flag) (!!((aux_data)->flags & flag))
 
 struct ble_ll_aux_data {
