0eda3935717241a33ca0ba4d5b57f823507b6388
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2019-01-29 17:39:39 +0100

    nimble/ll: Fix setting pending_ctrl_procs

--- nimble/controller/src/ble_ll_conn_hci.c
+++ nimble/controller/src/ble_ll_conn_hci.c
@@ -1608,7 +1608,7 @@ ble_ll_conn_hci_le_set_phy(uint8_t *cmdbuf)
          * pending bit but do not start the control procedure.
          */
         if (CONN_F_PEER_PHY_UPDATE(connsm)) {
-            connsm->pending_ctrl_procs |= BLE_LL_CTRL_PROC_PHY_UPDATE;
+            connsm->pending_ctrl_procs |= (1 << BLE_LL_CTRL_PROC_PHY_UPDATE);
             CONN_F_HOST_PHY_UPDATE(connsm) = 1;
         } else {
             /* Check if we should start phy update procedure */
