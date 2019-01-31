c31a54a792638e5d97e68585c8ccec5ae78edfee
Szymon Janc <szymon.janc@codecoup.pl>
2019-01-31 14:26:44 +0100

    nimble/ll: Move CSA2 and channel map functions to utils
    Those will be also used in periodic advertising.

--- nimble/controller/src/ble_ll_conn.c
+++ nimble/controller/src/ble_ll_conn.c
@@ -40,6 +40,7 @@
 #include "controller/ble_ll_trace.h"
 #include "controller/ble_phy.h"
 #include "controller/ble_hw.h"
+#include "controller/ble_ll_utils.h"
 #include "ble_ll_conn_priv.h"
 
 #if (BLETEST_THROUGHPUT_TEST == 1)
@@ -462,257 +463,6 @@ ble_ll_conn_calc_window_widening(struct ble_ll_conn_sm *connsm)
     return window_widening;
 }
 
-/**
- * Calculates the number of used channels in the channel map
- *
- * @param chmap
- *
- * @return uint8_t Number of used channels
- */
-uint8_t
-ble_ll_conn_calc_used_chans(uint8_t *chmap)
-{
-    int i;
-    int j;
-    uint8_t mask;
-    uint8_t chanbyte;
-    uint8_t used_channels;
-
-    used_channels = 0;
-    for (i = 0; i < BLE_LL_CONN_CHMAP_LEN; ++i) {
-        chanbyte = chmap[i];
-        if (chanbyte) {
-            if (chanbyte == 0xff) {
-                used_channels += 8;
-            } else {
-                mask = 0x01;
-                for (j = 0; j < 8; ++j) {
-                    if (chanbyte & mask) {
-                        ++used_channels;
-                    }
-                    mask <<= 1;
-                }
-            }
-        }
-    }
-    return used_channels;
-}
-
-static uint32_t
-ble_ll_conn_calc_access_addr(void)
-{
-    uint32_t aa;
-    uint16_t aa_low;
-    uint16_t aa_high;
-    uint32_t temp;
-    uint32_t mask;
-    uint32_t prev_bit;
-    uint8_t bits_diff;
-    uint8_t consecutive;
-    uint8_t transitions;
-    uint8_t ones;
-    int tmp;
-
-    /* Calculate a random access address */
-    aa = 0;
-    while (1) {
-        /* Get two, 16-bit random numbers */
-        aa_low = rand() & 0xFFFF;
-        aa_high = rand() & 0xFFFF;
-
-        /* All four bytes cannot be equal */
-        if (aa_low == aa_high) {
-            continue;
-        }
-
-        /* Upper 6 bits must have 2 transitions */
-        tmp = (int16_t)aa_high >> 10;
-        if (__builtin_popcount(tmp ^ (tmp >> 1)) < 2) {
-            continue;
-        }
-
-        /* Cannot be access address or be 1 bit different */
-        aa = aa_high;
-        aa = (aa << 16) | aa_low;
-        bits_diff = 0;
-        temp = aa ^ BLE_ACCESS_ADDR_ADV;
-        for (mask = 0x00000001; mask != 0; mask <<= 1) {
-            if (mask & temp) {
-                ++bits_diff;
-                if (bits_diff > 1) {
-                    break;
-                }
-            }
-        }
-        if (bits_diff <= 1) {
-            continue;
-        }
-
-        /* Cannot have more than 24 transitions */
-        transitions = 0;
-        consecutive = 1;
-        ones = 0;
-        mask = 0x00000001;
-        while (mask < 0x80000000) {
-            prev_bit = aa & mask;
-            mask <<= 1;
-            if (mask & aa) {
-                if (prev_bit == 0) {
-                    ++transitions;
-                    consecutive = 1;
-                } else {
-                    ++consecutive;
-                }
-            } else {
-                if (prev_bit == 0) {
-                    ++consecutive;
-                } else {
-                    ++transitions;
-                    consecutive = 1;
-                }
-            }
-
-            if (prev_bit) {
-                ones++;
-            }
-
-            /* 8 lsb should have at least three 1 */
-            if (mask == 0x00000100 && ones < 3) {
-                break;
-            }
-
-            /* 16 lsb should have no more than 11 transitions */
-            if (mask == 0x00010000 && transitions > 11) {
-                break;
-            }
-
-            /* This is invalid! */
-            if (consecutive > 6) {
-                /* Make sure we always detect invalid sequence below */
-                mask = 0;
-                break;
-            }
-        }
-
-        /* Invalid sequence found */
-        if (mask != 0x80000000) {
-            continue;
-        }
-
-        /* Cannot be more than 24 transitions */
-        if (transitions > 24) {
-            continue;
-        }
-
-        /* We have a valid access address */
-        break;
-    }
-    return aa;
-}
-
-static uint8_t
-ble_ll_conn_remapped_channel(uint8_t remap_index, const uint8_t *chanmap)
-{
-    uint8_t cntr;
-    uint8_t mask;
-    uint8_t usable_chans;
-    uint8_t chan;
-    int i, j;
-
-    /* NOTE: possible to build a map but this would use memory. For now,
-       we just calculate */
-    /* Iterate through channel map to find this channel */
-    chan = 0;
-    cntr = 0;
-    for (i = 0; i < BLE_LL_CONN_CHMAP_LEN; i++) {
-        usable_chans = chanmap[i];
-        if (usable_chans != 0) {
-            mask = 0x01;
-            for (j = 0; j < 8; j++) {
-                if (usable_chans & mask) {
-                    if (cntr == remap_index) {
-                        return (chan + j);
-                    }
-                    ++cntr;
-                }
-                mask <<= 1;
-            }
-        }
-        chan += 8;
-    }
-
-    /* we should never reach here */
-    BLE_LL_ASSERT(0);
-    return 0;
-}
-
-#if (MYNEWT_VAL(BLE_LL_CFG_FEAT_LE_CSA2) == 1)
-static uint16_t
-ble_ll_conn_csa2_perm(uint16_t in)
-{
-    uint16_t out = 0;
-    int i;
-
-    for (i = 0; i < 8; i++) {
-        out |= ((in >> i) & 0x00000001) << (7 - i);
-    }
-
-    for (i = 8; i < 16; i++) {
-        out |= ((in >> i) & 0x00000001) << (15 + 8 - i);
-    }
-
-    return out;
-}
-
-static uint16_t
-ble_ll_conn_csa2_prng(uint16_t counter, uint16_t ch_id)
-{
-    uint16_t prn_e;
-
-    prn_e = counter ^ ch_id;
-
-    prn_e = ble_ll_conn_csa2_perm(prn_e);
-    prn_e = (prn_e * 17) + ch_id;
-
-    prn_e = ble_ll_conn_csa2_perm(prn_e);
-    prn_e = (prn_e * 17) + ch_id;
-
-    prn_e = ble_ll_conn_csa2_perm(prn_e);
-    prn_e = (prn_e * 17) + ch_id;
-
-    prn_e = prn_e ^ ch_id;
-
-    return prn_e;
-}
-
-static uint8_t
-ble_ll_conn_calc_dci_csa2(struct ble_ll_conn_sm *conn)
-{
-    uint16_t channel_unmapped;
-    uint8_t remap_index;
-
-    uint16_t prn_e;
-    uint8_t bitpos;
-
-    prn_e = ble_ll_conn_csa2_prng(conn->event_cntr, conn->channel_id);
-
-    channel_unmapped = prn_e % 37;
-
-    /*
-     * If unmapped channel is the channel index of a used channel it is used
-     * as channel index.
-     */
-    bitpos = 1 << (channel_unmapped & 0x07);
-    if (conn->chanmap[channel_unmapped >> 3] & bitpos) {
-        return channel_unmapped;
-    }
-
-    remap_index = (conn->num_used_chans * prn_e) / 0x10000;
-
-    return ble_ll_conn_remapped_channel(remap_index, conn->chanmap);
-}
-#endif
-
 static uint8_t
 ble_ll_conn_calc_dci_csa1(struct ble_ll_conn_sm *conn)
 {
@@ -738,7 +488,7 @@ ble_ll_conn_calc_dci_csa1(struct ble_ll_conn_sm *conn)
     /* Calculate remap index */
     remap_index = curchan % conn->num_used_chans;
 
-    return ble_ll_conn_remapped_channel(remap_index, conn->chanmap);
+    return ble_ll_utils_remapped_channel(remap_index, conn->chanmap);
 }
 
 /**
@@ -757,7 +507,8 @@ ble_ll_conn_calc_dci(struct ble_ll_conn_sm *conn, uint16_t latency)
 
 #if (MYNEWT_VAL(BLE_LL_CFG_FEAT_LE_CSA2) == 1)
     if (CONN_F_CSA2_SUPP(conn)) {
-        return ble_ll_conn_calc_dci_csa2(conn);
+        return ble_ll_utils_calc_dci_csa2(conn->event_cntr, conn->channel_id,
+                                          conn->num_used_chans, conn->chanmap);
     }
 #endif
 
@@ -1722,7 +1473,7 @@ ble_ll_conn_master_common_init(struct ble_ll_conn_sm *connsm)
            BLE_LL_CONN_CHMAP_LEN);
 
     /*  Calculate random access address and crc initialization value */
-    connsm->access_addr = ble_ll_conn_calc_access_addr();
+    connsm->access_addr = ble_ll_utils_calc_access_addr();
     connsm->crcinit = rand() & 0xffffff;
 
     /* Set initial schedule callback */
@@ -2322,7 +2073,7 @@ ble_ll_conn_next_event(struct ble_ll_conn_sm *connsm)
          * transmitted update request. Would end up killing connection
            on slave side. Could ignore it or see if still enqueued. */
         connsm->num_used_chans =
-            ble_ll_conn_calc_used_chans(connsm->req_chanmap);
+            ble_ll_utils_calc_num_used_chans(connsm->req_chanmap);
         memcpy(connsm->chanmap, connsm->req_chanmap, BLE_LL_CONN_CHMAP_LEN);
 
         connsm->csmflags.cfbit.chanmap_update_scheduled = 0;
@@ -4281,7 +4032,7 @@ ble_ll_conn_slave_start(uint8_t *rxbuf, uint8_t pat, struct ble_mbuf_hdr *rxhdr,
     connsm->peer_addr_type = pat;
 
     /* Calculate number of used channels; make sure it meets min requirement */
-    connsm->num_used_chans = ble_ll_conn_calc_used_chans(connsm->chanmap);
+    connsm->num_used_chans = ble_ll_utils_calc_num_used_chans(connsm->chanmap);
     if (connsm->num_used_chans < 2) {
         goto err_slave_start;
     }
