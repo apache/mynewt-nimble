3dac1c574a103b9ef16007b4c73528a7345cef78
mnasr93 <muhammadmostafa0@gmail.com>
2019-04-10 22:57:05 +0200

    nimble/gap: Add listener call to notify_rx event

--- nimble/host/src/ble_gap.c
+++ nimble/host/src/ble_gap.c
@@ -5171,6 +5171,7 @@ ble_gap_notify_rx_event(uint16_t conn_handle, uint16_t attr_handle,
     event.notify_rx.attr_handle = attr_handle;
     event.notify_rx.om = om;
     event.notify_rx.indication = is_indication;
+    ble_gap_event_listener_call(&event);
     ble_gap_call_conn_event_cb(&event, conn_handle);
 
     os_mbuf_free_chain(event.notify_rx.om);
