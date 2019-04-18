apache/mynewt-nimble

commit 4a8946786e24c593da117a458007f7a75269baef

nimble/hci: Modifying ACL Buf size based on BLE 5.1 Standard

nimble/host/src/ble_hs_hci.c
@@ -407,13 +407,17 @@ ble_hs_hci_rx_evt(uint8_t *hci_ev, void *arg)
 }
 
 /**
- * Calculates the largest ACL payload that the controller can accept.  This is
- * everything in an ACL data packet except for the ACL header.
+ * Calculates the largest ACL payload that the controller can accept.
  */
 static uint16_t
 ble_hs_hci_max_acl_payload_sz(void)
 {
-    return ble_hs_hci_buf_sz - BLE_HCI_DATA_HDR_SZ;
+    /* As per BLE 5.1 Standard, Vol. 2, Part E, section 7.8.2:
+     * The LE_Read_Buffer_Size command is used to read the maximum size of the
+     * data portion of HCI LE ACL Data Packets sent from the Host to the
+     * Controller.
+     */
+    return ble_hs_hci_buf_sz;
 }
 
 /**

commit 18aec10e60d9b27ca3c811330a1851d7d2323e15

nimble/host: Fix HCI fragmentation buffer size
This patch fixes a regression introduced in:
4a894678 nimble/hci: Modifying ACL Buf size based on BLE 5.1 Standard

and is related to:
2bd8de56 nimble/host: Register a 1-elem mbuf pool for ACL

After adding a dedicated mbuf pool for HCI fragmentation
and changing the ACL buffer size, the fragmentation data buffer
size was too small to hold full ACL buffer including HCI header.

nimble/host/src/ble_hs_hci.c
@@ -41,6 +41,7 @@ static uint8_t ble_hs_hci_version;
 
 #define BLE_HS_HCI_FRAG_DATABUF_SIZE    \
     (BLE_ACL_MAX_PKT_SIZE +             \
+     BLE_HCI_DATA_HDR_SZ +              \
      sizeof (struct os_mbuf_pkthdr) +   \
      sizeof (struct os_mbuf))
 