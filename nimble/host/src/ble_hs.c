ba0d2098ac6d8f087284b7f59876ca9b5197a553
Szymon Janc <szymon.janc@codecoup.pl>
2019-10-09 10:27:25 +0200

    nimble/host: Build ble_hs_periodic_sync only when enabled
    When periodic advertising support is disabled this code is never used.

--- nimble/host/src/ble_hs.c
+++ nimble/host/src/ble_hs.c
@@ -743,8 +743,10 @@ ble_hs_init(void)
     rc = ble_hs_conn_init();
     SYSINIT_PANIC_ASSERT(rc == 0);
 
+#if MYNEWT_VAL(BLE_PERIODIC_ADV)
     rc = ble_hs_periodic_sync_init();
     SYSINIT_PANIC_ASSERT(rc == 0);
+#endif
 
     rc = ble_l2cap_init();
     SYSINIT_PANIC_ASSERT(rc == 0);
