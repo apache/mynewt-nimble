feb590932d3287efd888874fe3ef08b6b4ad1e5a
Michał Narajowski <michal.narajowski@codecoup.pl>
2019-11-12 16:31:56 +0100

    porting: Update modlog.h file to use IGNORE macro
    This macro is used in official modlog package so since
    we want to use logcfg in porting, we can also use IGNORE macro.

--- porting/nimble/include/modlog/modlog.h
+++ porting/nimble/include/modlog/modlog.h
@@ -26,58 +26,42 @@
 
 #define MODLOG_MODULE_DFLT 255
 
-#if (MYNEWT_VAL(LOG_LEVEL) > 0)
-static inline void
-modlog_dummy(const char *msg, ...)
-{
-    (void)msg;
-}
-#endif
-
-#if (MYNEWT_VAL(LOG_LEVEL) > 0)
+#if MYNEWT_VAL(LOG_LEVEL) <= LOG_LEVEL_DEBUG || defined __DOXYGEN__
 #define MODLOG_DEBUG(ml_mod_, ml_msg_, ...) \
-        modlog_dummy((ml_msg_), ##__VA_ARGS__)
+    printf((ml_msg_), ##__VA_ARGS__)
 #else
-#define MODLOG_DEBUG(ml_mod_, ml_msg_, ...) \
-        printf((ml_msg_), ##__VA_ARGS__);
+#define MODLOG_DEBUG(ml_mod_, ...) IGNORE(__VA_ARGS__)
 #endif
 
-#if (MYNEWT_VAL(LOG_LEVEL) > 1)
+#if MYNEWT_VAL(LOG_LEVEL) <= LOG_LEVEL_INFO || defined __DOXYGEN__
 #define MODLOG_INFO(ml_mod_, ml_msg_, ...) \
-        modlog_dummy((ml_msg_), ##__VA_ARGS__)
+    printf((ml_msg_), ##__VA_ARGS__)
 #else
-#define MODLOG_INFO(ml_mod_, ml_msg_, ...) \
-        printf((ml_msg_), ##__VA_ARGS__);
+#define MODLOG_INFO(ml_mod_, ...) IGNORE(__VA_ARGS__)
 #endif
 
-#if (MYNEWT_VAL(LOG_LEVEL) > 2)
+#if MYNEWT_VAL(LOG_LEVEL) <= LOG_LEVEL_WARN || defined __DOXYGEN__
 #define MODLOG_WARN(ml_mod_, ml_msg_, ...) \
-        modlog_dummy((ml_msg_), ##__VA_ARGS__)
+    printf((ml_msg_), ##__VA_ARGS__)
 #else
-#define MODLOG_WARN(ml_mod_, ml_msg_, ...) \
-        printf((ml_msg_), ##__VA_ARGS__);
+#define MODLOG_WARN(ml_mod_, ...) IGNORE(__VA_ARGS__)
 #endif
 
-#if (MYNEWT_VAL(LOG_LEVEL) > 3)
+#if MYNEWT_VAL(LOG_LEVEL) <= LOG_LEVEL_ERROR || defined __DOXYGEN__
 #define MODLOG_ERROR(ml_mod_, ml_msg_, ...) \
-        modlog_dummy((ml_msg_), ##__VA_ARGS__)
+    printf((ml_msg_), ##__VA_ARGS__)
 #else
-#define MODLOG_ERROR(ml_mod_, ml_msg_, ...) \
-        printf((ml_msg_), ##__VA_ARGS__);
+#define MODLOG_ERROR(ml_mod_, ...) IGNORE(__VA_ARGS__)
 #endif
 
-#if (MYNEWT_VAL(LOG_LEVEL) > 4)
+#if MYNEWT_VAL(LOG_LEVEL) <= LOG_LEVEL_CRITICAL || defined __DOXYGEN__
 #define MODLOG_CRITICAL(ml_mod_, ml_msg_, ...) \
-        modlog_dummy((ml_msg_), ##__VA_ARGS__)
+    printf((ml_msg_), ##__VA_ARGS__)
 #else
-#define MODLOG_CRITICAL(ml_mod_, ml_msg_, ...) \
-        printf((ml_msg_), ##__VA_ARGS__);
+#define MODLOG_CRITICAL(ml_mod_, ...) IGNORE(__VA_ARGS__)
 #endif
 
 #define MODLOG(ml_lvl_, ml_mod_, ...) \
     MODLOG_ ## ml_lvl_((ml_mod_), __VA_ARGS__)
-
-#define MODLOG_DFLT(ml_lvl_, ...) \
-    MODLOG(ml_lvl_, LOG_MODULE_DEFAULT, __VA_ARGS__)
 
 #endif
