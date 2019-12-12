fd867ee8eb66dbbd3e934c71e6ef3d32755a5fec
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2019-12-12 19:02:52 +0100

    nimble/ll: Fix packet transmit time restrictions
    When determining TX PHY for transition we should select the one with
    with the longest transmission time to make sure we conform to time
    restrictions on target PHY during transition.
    
    Reference: Core 5.1, Vol 6, Part B, 5.1.10.1

--- nimble/controller/include/controller/ble_ll_ctrl.h
+++ nimble/controller/include/controller/ble_ll_ctrl.h
@@ -303,6 +303,7 @@ void ble_ll_ctrl_phy_update_proc_complete(struct ble_ll_conn_sm *connsm);
 void ble_ll_ctrl_initiate_dle(struct ble_ll_conn_sm *connsm);
 void ble_ll_hci_ev_send_vendor_err(const char *file, uint32_t line);
 
+uint8_t ble_ll_ctrl_phy_tx_transition_get(uint8_t phy_mask);
 uint8_t ble_ll_ctrl_phy_from_phy_mask(uint8_t phy_mask);
 
 #ifdef __cplusplus
