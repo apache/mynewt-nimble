dd4e011ed6242d1e7fde78b3f582dfd42056177d
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2020-02-12 21:37:02 +0100

    nimble/host: Make gap init before att
    This is needed as eatt is registering listener in gap nowadays

--- nimble/host/src/ble_hs.c
+++ nimble/host/src/ble_hs.c
@@ -734,6 +734,11 @@ ble_hs_init(void)
     rc = ble_l2cap_init();
     SYSINIT_PANIC_ASSERT(rc == 0);
 
+#endif
+    rc = ble_gap_init();
+    SYSINIT_PANIC_ASSERT(rc == 0);
+#if NIMBLE_BLE_CONNECT
+
     rc = ble_att_init();
     SYSINIT_PANIC_ASSERT(rc == 0);
 
@@ -746,8 +751,6 @@ ble_hs_init(void)
     rc = ble_gatts_init();
     SYSINIT_PANIC_ASSERT(rc == 0);
 #endif
-    rc = ble_gap_init();
-    SYSINIT_PANIC_ASSERT(rc == 0);
 
     ble_hs_stop_init();
 
