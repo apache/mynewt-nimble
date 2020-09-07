3431c13fae3ce5cdbd29bc5a37635a47e6d6d8ba
Krzysztof Kopyściński <krzysztof.kopyscinski@codecoup.pl>
2020-09-07 10:21:58 +0200

    Bluetooth: Mesh: Heartbeat period starts at tx Starts the periodic heartbeat publish period at the end of the publication, instead of at the ordering time. This ensures that the heartbeat period doesn't get shortened by other enqueued messages.

--- nimble/host/mesh/src/lpn.c
+++ nimble/host/mesh/src/lpn.c
@@ -243,7 +243,7 @@ static void clear_friendship(bool force, bool disable)
 	lpn->groups_changed = 1;
 
 	if (cfg->hb_pub.feat & BT_MESH_FEAT_LOW_POWER) {
-		bt_mesh_heartbeat_send();
+		(void)bt_mesh_heartbeat_send(NULL, NULL);
 	}
 
 	if (disable) {
@@ -961,7 +961,7 @@ int bt_mesh_lpn_friend_update(struct bt_mesh_net_rx *rx,
 		BT_INFO("Friendship established with 0x%04x", lpn->frnd);
 
 		if (cfg->hb_pub.feat & BT_MESH_FEAT_LOW_POWER) {
-			bt_mesh_heartbeat_send();
+			(void)bt_mesh_heartbeat_send(NULL, NULL);
 		}
 
 		if (lpn_cb) {
