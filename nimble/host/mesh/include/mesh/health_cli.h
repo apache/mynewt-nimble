d53adf573a02becf76f33d3b486481b8dde04d8c
Krzysztof Kopyściński <krzysztof.kopyscinski@codecoup.pl>
2020-11-03 10:00:34 +0100

    mesh: Remove net_idx params when with app key
    According Mesh Profile 1.0.1. A application key shall
    binding single network key. And Device key shall bind all
    network key, and dev key only known by cfg_cli and node self,
    only used by cfg_cli & cfg_srv.
    
    this is port of 7027f197a4a7586804c02843a0de4720aab2a765 and
    9eda79cd13936fefb144bdbb02f054ed0dfd93be

--- nimble/host/mesh/include/mesh/health_cli.h
+++ nimble/host/mesh/include/mesh/health_cli.h
@@ -43,29 +43,27 @@ extern const struct bt_mesh_model_cb bt_mesh_health_cli_cb;
 
 int bt_mesh_health_cli_set(struct bt_mesh_model *model);
 
-int bt_mesh_health_fault_get(u16_t net_idx, u16_t addr, u16_t app_idx,
-			     u16_t cid, u8_t *test_id, u8_t *faults,
-			     size_t *fault_count);
+int bt_mesh_health_fault_get(u16_t addr, u16_t app_idx, u16_t cid,
+							 u8_t *test_id, u8_t *faults,
+							 size_t *fault_count);
 
-int bt_mesh_health_fault_clear(u16_t net_idx, u16_t addr, u16_t app_idx,
-			       u16_t cid, u8_t *test_id, u8_t *faults,
-			       size_t *fault_count);
+int bt_mesh_health_fault_clear(u16_t addr, u16_t app_idx, u16_t cid,
+							   u8_t *test_id, u8_t *faults,
+							   size_t *fault_count);
 
-int bt_mesh_health_fault_test(u16_t net_idx, u16_t addr, u16_t app_idx,
-			      u16_t cid, u8_t test_id, u8_t *faults,
-			      size_t *fault_count);
+int bt_mesh_health_fault_test(u16_t addr, u16_t app_idx, u16_t cid,
+							  u8_t test_id, u8_t *faults,
+							  size_t *fault_count);
 
-int bt_mesh_health_period_get(u16_t net_idx, u16_t addr, u16_t app_idx,
-			      u8_t *divisor);
+int bt_mesh_health_period_get(u16_t addr, u16_t app_idx, u8_t *divisor);
 
-int bt_mesh_health_period_set(u16_t net_idx, u16_t addr, u16_t app_idx,
-			      u8_t divisor, u8_t *updated_divisor);
+int bt_mesh_health_period_set(u16_t addr, u16_t app_idx, u8_t divisor,
+							  u8_t *updated_divisor);
 
-int bt_mesh_health_attention_get(u16_t net_idx, u16_t addr, u16_t app_idx,
-				 u8_t *attention);
+int bt_mesh_health_attention_get(u16_t addr, u16_t app_idx, u8_t *attention);
 
-int bt_mesh_health_attention_set(u16_t net_idx, u16_t addr, u16_t app_idx,
-				 u8_t attention, u8_t *updated_attention);
+int bt_mesh_health_attention_set(u16_t addr, u16_t app_idx, u8_t attention,
+								 u8_t *updated_attention);
 
 s32_t bt_mesh_health_cli_timeout_get(void);
 void bt_mesh_health_cli_timeout_set(s32_t timeout);
