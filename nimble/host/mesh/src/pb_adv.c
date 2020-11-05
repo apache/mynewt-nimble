e08f2f3da186a4b811071071e8a3f5f5492f277c
Krzysztof Kopyściński <krzysztof.kopyscinski@codecoup.pl>
2020-11-05 09:44:04 +0100

    mesh: Remove redundant expression
    Remove checking of the same argument twice
    
    this is port of 21c37a6c6a6feea493836f4b6d8e690e1ef724bb

--- nimble/host/mesh/src/pb_adv.c
+++ nimble/host/mesh/src/pb_adv.c
@@ -397,7 +397,7 @@ static void gen_prov_ack(struct prov_rx *rx, struct os_mbuf *buf)
 			prov_clear_tx();
 		}
 
-		if (link.tx.cb && link.tx.cb) {
+		if (link.tx.cb) {
 			link.tx.cb(0, link.tx.cb_data);
 		}
 	}
