971633179581faff002d906c7a5d0e168b4236cd
Francisco Molina <femolina@uc.cl>
2021-10-28 15:37:54 +0200

    nimble/host/include/host/ble_gap: add ble_gap_ext_adv_active()

--- nimble/host/src/ble_gap.c
+++ nimble/host/src/ble_gap.c
@@ -1303,6 +1303,16 @@ ble_gap_adv_active_instance(uint8_t instance)
 }
 #endif
 
+#if MYNEWT_VAL(BLE_EXT_ADV)
+int ble_gap_ext_adv_active(uint8_t instance)
+{
+    if (instance >= BLE_ADV_INSTANCES) {
+        return 0;
+    }
+    return ble_gap_adv_active_instance(instance);
+}
+#endif
+
 /**
  * Clears advertisement and discovery state.  This function is necessary
  * when the controller loses its active state (e.g. on stack reset).
