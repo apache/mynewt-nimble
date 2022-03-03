d5201cb83bf0a5f3b9788b0721b656fb22a96223
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2022-03-03 18:07:56 +0100

    nimble/transport: Add common code for new HCI transport

---
+++ nimble/transport/include/nimble/transport.h
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef H_NIMBLE_TRANSPORT_
+#define H_NIMBLE_TRANSPORT_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct os_mbuf;
+
+/* Init functions to be implemented for transport acting as HS/LL side */
+extern void ble_transport_ll_init(void);
+extern void ble_transport_hs_init(void);
+
+/* APIs to be implemented by HS/LL side of transports */
+extern int ble_transport_to_ll_cmd(void *buf);
+extern int ble_transport_to_ll_acl(struct os_mbuf *om);
+extern int ble_transport_to_hs_evt(void *buf);
+extern int ble_transport_to_hs_acl(struct os_mbuf *om);
+
+/* Allocators for supported data types */
+void *ble_transport_alloc_cmd(void);
+void *ble_transport_alloc_evt(int discardable);
+struct os_mbuf *ble_transport_alloc_acl_from_hs(void);
+struct os_mbuf *ble_transport_alloc_acl_from_ll(void);
+
+/* Generic deallocator for cmd/evt buffers */
+void ble_transport_free(void *buf);
+
+/* Register put callback on acl_from_ll mbufs (for ll-hs flow control) */
+int ble_transport_register_put_acl_from_ll_cb(os_mempool_put_fn *cb);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* H_NIMBLE_TRANSPORT_ */
---
+++ nimble/transport/src/transport.c
@@ -0,0 +1,217 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <stdint.h>
+#include <syscfg/syscfg.h>
+#include <sysinit/sysinit.h>
+#include <os/os_mbuf.h>
+#include <os/os_mempool.h>
+#include <nimble/ble.h>
+#include <nimble/hci_common.h>
+#include <nimble/transport.h>
+
+#define OMP_FLAG_FROM_HS        (0x01)
+#define OMP_FLAG_FROM_LL        (0x02)
+#define OMP_FLAG_FROM_MASK      (0x03)
+
+#if MYNEWT_VAL(BLE_HS_FLOW_CTRL) || \
+    MYNEWT_VAL(BLE_LL_CFG_FEAT_CTRL_TO_HOST_FLOW_CONTROL)
+#define POOL_CMD_COUNT      (2)
+#else
+#define POOL_CMD_COUNT      (1)
+#endif
+#define POOL_CMD_SIZE       (258)
+
+#define POOL_EVT_COUNT      (MYNEWT_VAL(BLE_TRANSPORT_EVT_COUNT))
+#define POOL_EVT_LO_COUNT   (MYNEWT_VAL(BLE_TRANSPORT_EVT_DISCARDABLE_COUNT))
+#define POOL_EVT_SIZE       (MYNEWT_VAL(BLE_TRANSPORT_EVT_SIZE))
+
+#if MYNEWT_VAL_CHOICE(BLE_TRANSPORT_LL, native) && \
+   MYNEWT_VAL_CHOICE(BLE_TRANSPORT_HS, native)
+#define POOL_ACL_COUNT      (0)
+#elif !MYNEWT_VAL_CHOICE(BLE_TRANSPORT_LL, native) && \
+      !MYNEWT_VAL_CHOICE(BLE_TRANSPORT_HS, native)
+#define POOL_ACL_COUNT      ((MYNEWT_VAL(BLE_TRANSPORT_ACL_FROM_HS_COUNT)) + \
+                             (MYNEWT_VAL(BLE_TRANSPORT_ACL_FROM_LL_COUNT)))
+#elif MYNEWT_VAL_CHOICE(BLE_TRANSPORT_LL, native)
+#define POOL_ACL_COUNT      (MYNEWT_VAL(BLE_TRANSPORT_ACL_FROM_HS_COUNT))
+#else
+#define POOL_ACL_COUNT      (MYNEWT_VAL(BLE_TRANSPORT_ACL_FROM_LL_COUNT))
+#endif
+#define POOL_ACL_SIZE       (OS_ALIGN( MYNEWT_VAL(BLE_TRANSPORT_ACL_SIZE) + \
+                                       BLE_MBUF_MEMBLOCK_OVERHEAD +         \
+                                       BLE_HCI_DATA_HDR_SZ, OS_ALIGNMENT))
+
+static uint8_t pool_cmd_buf[ OS_MEMPOOL_BYTES(POOL_CMD_COUNT, POOL_CMD_SIZE) ];
+static struct os_mempool pool_cmd;
+
+static uint8_t pool_evt_buf[ OS_MEMPOOL_BYTES(POOL_EVT_COUNT, POOL_EVT_SIZE) ];
+static struct os_mempool pool_evt;
+
+static uint8_t pool_evt_lo_buf[ OS_MEMPOOL_BYTES(POOL_EVT_LO_COUNT, POOL_EVT_SIZE) ];
+static struct os_mempool pool_evt_lo;
+
+static uint8_t pool_acl_buf[ OS_MEMPOOL_BYTES(POOL_ACL_COUNT, POOL_ACL_SIZE) ];
+static struct os_mempool_ext pool_acl;
+static struct os_mbuf_pool mpool_acl;
+
+static os_mempool_put_fn *transport_put_acl_from_ll_cb;
+
+void *
+ble_transport_alloc_cmd(void)
+{
+    return os_memblock_get(&pool_cmd);
+}
+
+void *
+ble_transport_alloc_evt(int discarcable)
+{
+    void *buf;
+
+    if (discarcable) {
+        buf = os_memblock_get(&pool_evt_lo);
+    } else {
+        buf = os_memblock_get(&pool_evt);
+        if (!buf) {
+            buf = os_memblock_get(&pool_evt_lo);
+        }
+    }
+
+    return buf;
+}
+
+struct os_mbuf *
+ble_transport_alloc_acl_from_hs(void)
+{
+    struct os_mbuf *om;
+    struct os_mbuf_pkthdr *pkthdr;
+    uint16_t usrhdr_len;
+
+#if MYNEWT_VAL_CHOICE(BLE_TRANSPORT_LL, native)
+    usrhdr_len = sizeof(struct ble_mbuf_hdr);
+#else
+    usrhdr_len = 0;
+#endif
+
+    om = os_mbuf_get_pkthdr(&mpool_acl, usrhdr_len);
+    if (om) {
+        pkthdr = OS_MBUF_PKTHDR(om);
+        pkthdr->omp_flags = OMP_FLAG_FROM_HS;
+    }
+
+    return om;
+}
+
+struct os_mbuf *
+ble_transport_alloc_acl_from_ll(void)
+{
+    struct os_mbuf *om;
+    struct os_mbuf_pkthdr *pkthdr;
+
+    om = os_mbuf_get_pkthdr(&mpool_acl, 0);
+    if (om) {
+        pkthdr = OS_MBUF_PKTHDR(om);
+        pkthdr->omp_flags = OMP_FLAG_FROM_LL;
+    }
+
+    return om;
+}
+
+void
+ble_transport_free(void *buf)
+{
+    if (os_memblock_from(&pool_cmd, buf)) {
+        os_memblock_put(&pool_cmd, buf);
+    } else if (os_memblock_from(&pool_evt, buf)) {
+        os_memblock_put(&pool_evt, buf);
+    } else if (os_memblock_from(&pool_evt_lo, buf)) {
+        os_memblock_put(&pool_evt_lo, buf);
+    } else {
+        assert(0);
+    }
+}
+
+static os_error_t
+ble_transport_acl_put(struct os_mempool_ext *mpe, void *data, void *arg)
+{
+    struct os_mbuf *om;
+    struct os_mbuf_pkthdr *pkthdr;
+    os_error_t err;
+
+    om = data;
+    pkthdr = OS_MBUF_PKTHDR(om);
+
+    switch (pkthdr->omp_flags & OMP_FLAG_FROM_MASK) {
+    case OMP_FLAG_FROM_LL:
+        if (transport_put_acl_from_ll_cb) {
+            return transport_put_acl_from_ll_cb(mpe, data, arg);
+        }
+        break;
+    case OMP_FLAG_FROM_HS:
+        break;
+    default:
+        assert(0);
+        break;
+    }
+
+    err = os_memblock_put_from_cb(&mpe->mpe_mp, data);
+    if (err) {
+        return err;
+    }
+
+    return 0;
+}
+
+void
+ble_transport_init(void)
+{
+    int rc;
+
+    SYSINIT_ASSERT_ACTIVE();
+
+    rc = os_mempool_init(&pool_cmd, POOL_CMD_COUNT, POOL_CMD_SIZE,
+                         pool_cmd_buf, "transport_pool_cmd");
+    SYSINIT_PANIC_ASSERT(rc == 0);
+
+    rc = os_mempool_init(&pool_evt, POOL_EVT_COUNT, POOL_EVT_SIZE,
+                         pool_evt_buf, "transport_pool_evt");
+    SYSINIT_PANIC_ASSERT(rc == 0);
+
+    rc = os_mempool_init(&pool_evt_lo, POOL_EVT_LO_COUNT, POOL_EVT_SIZE,
+                         pool_evt_lo_buf, "transport_pool_evt_lo");
+    SYSINIT_PANIC_ASSERT(rc == 0);
+
+    rc = os_mempool_ext_init(&pool_acl, POOL_ACL_COUNT, POOL_ACL_SIZE,
+                             pool_acl_buf, "transport_pool_acl");
+    SYSINIT_PANIC_ASSERT(rc == 0);
+
+    rc = os_mbuf_pool_init(&mpool_acl, &pool_acl.mpe_mp,
+                           POOL_ACL_SIZE, POOL_ACL_COUNT);
+    SYSINIT_PANIC_ASSERT(rc == 0);
+
+    pool_acl.mpe_put_cb = ble_transport_acl_put;
+}
+
+int
+ble_transport_register_put_acl_from_ll_cb(os_mempool_put_fn (*cb))
+{
+    transport_put_acl_from_ll_cb = cb;
+
+    return 0;
+}
