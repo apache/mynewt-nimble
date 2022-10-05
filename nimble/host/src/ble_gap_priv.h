717b58c0a3deaa5a540693f0213c6444a82b1f36
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2022-10-05 11:42:07 +0200

    nimble/host: Add APIs to handle vs HCI commands and events
    This adds API to send a vs hci command and an event to handle vs hci
    events.

--- nimble/host/src/ble_gap_priv.h
+++ nimble/host/src/ble_gap_priv.h
@@ -138,6 +138,7 @@ void ble_gap_mtu_event(uint16_t conn_handle, uint16_t cid, uint16_t mtu);
 void ble_gap_identity_event(uint16_t conn_handle);
 int ble_gap_repeat_pairing_event(const struct ble_gap_repeat_pairing *rp);
 void ble_gap_pairing_complete_event(uint16_t conn_handle, int status);
+void ble_gap_vs_hci_event(const void *buf, uint8_t len);
 int ble_gap_master_in_progress(void);
 
 void ble_gap_preempt(void);
