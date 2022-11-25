9f99d55e64d492057425b455810b465dca8efd5b
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2022-11-25 13:56:23 +0100

    nimble/drivers/fem: Add support for nRF21540
    This adds support for nRF21540 FEM.
    FEM is controlled via GPIOs, there's no support for SPI.

---
+++ nimble/drivers/fem/nrf21540/include/nrf21540/nrf21540.h
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+#ifndef _NRF21540_H
+#define _NRF21540_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int nrf21540_mode_set(uint8_t mode);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _NRF21540_H */
