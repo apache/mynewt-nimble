7eb94cb826f7c39bd97c25eefc4ea7665a487e10
Hang Fan <fanhang8@gmail.com>
2023-12-30 23:30:51 +0800

    porting/examples: fix ble_hci_sock_init assertion
    Since HCI socket was initialized by `ble_transport_ll_init` in LL side,
    we should remove `ble_hci_sock_init` from example apps.
    
    Signed-off-by: Hang Fan <fanhang8@gmail.com>

--- porting/examples/nuttx/main.c
+++ porting/examples/nuttx/main.c
@@ -43,7 +43,6 @@ static struct ble_npl_task s_task_hci;
 
 void nimble_host_task(void *param);
 void ble_hci_sock_ack_handler(void *param);
-void ble_hci_sock_init(void);
 void ble_hci_sock_set_device(int dev);
 void ble_store_ram_init(void);
 
@@ -88,9 +87,6 @@ int main(int argc, char *argv[])
 
     printf("port init\n");
     nimble_port_init();
-
-    printf("hci init\n");
-    ble_hci_sock_init();
 
     /* This example provides GATT Alert service */
     printf("gap init\n");
