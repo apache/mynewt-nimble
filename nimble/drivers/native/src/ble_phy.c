b26b6382b0af49adf26d7775a43d0980b03b998a
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2024-01-12 16:52:44 +0100

    nimble/phy: Fix compilation error
    This fixes compilation error caused by value assignment to undefined
    structure aux_data member. Introduced in 4e58b2724.

--- nimble/drivers/native/src/ble_phy.c
+++ nimble/drivers/native/src/ble_phy.c
@@ -281,7 +281,7 @@ ble_phy_isr(void)
         ble_hdr->rxinfo.channel = g_ble_phy_data.phy_chan;
         ble_hdr->rxinfo.phy = BLE_PHY_1M;
 #if MYNEWT_VAL(BLE_LL_CFG_FEAT_LL_EXT_ADV)
-        ble_hdr->rxinfo.aux_data = NULL;
+        ble_hdr->rxinfo.user_data = NULL;
 #endif
 
         /* Count PHY valid packets */
