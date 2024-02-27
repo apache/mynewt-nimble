56a23bc0feafd8726eea65863e882bb036d566af
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2024-02-27 14:08:39 +0100

    apps/btshell: Fix minor coding style issue
    This fixes minor coding style issue in btshell_scan_opts.

--- apps/btshell/src/btshell.h
+++ apps/btshell/src/btshell.h
@@ -90,8 +90,8 @@ struct btshell_conn {
 
 struct btshell_scan_opts {
     uint16_t limit;
-    uint8_t ignore_legacy:1;
-    uint8_t periodic_only:1;
+    uint8_t ignore_legacy : 1;
+    uint8_t periodic_only : 1;
     uint8_t name_filter_len;
     char name_filter[NAME_FILTER_LEN_MAX];
 };
