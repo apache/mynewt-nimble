41d852c8d4c8d0e1e2e2e134edf6d49bbcec93c2
Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>
2024-03-14 10:35:31 +0100

    nimble/host: Update Channel Sounding API to handle CS results
    Forward step results to the application layer.
    Update Channel Sounding HCI to the final release of Bluetooth spec 6.0.

--- nimble/host/src/ble_cs_priv.h
+++ nimble/host/src/ble_cs_priv.h
@@ -20,6 +20,22 @@
 #ifndef H_BLE_CS_PRIV_
 #define H_BLE_CS_PRIV_
 
+#define BLE_HS_CS_RTT_AA_ONLY                  (0x00)
+#define BLE_HS_CS_RTT_32_BIT_SOUNDING_SEQUENCE (0x01)
+#define BLE_HS_CS_RTT_96_BIT_SOUNDING_SEQUENCE (0x02)
+#define BLE_HS_CS_RTT_32_BIT_RANDOM_SEQUENCE   (0x03)
+#define BLE_HS_CS_RTT_64_BIT_RANDOM_SEQUENCE   (0x04)
+#define BLE_HS_CS_RTT_96_BIT_RANDOM_SEQUENCE   (0x05)
+#define BLE_HS_CS_RTT_128_BIT_RANDOM_SEQUENCE  (0x06)
+
+#define BLE_HS_CS_SUBEVENT_DONE_STATUS_COMPLETED (0x0)
+#define BLE_HS_CS_SUBEVENT_DONE_STATUS_PARTIAL   (0x1)
+#define BLE_HS_CS_SUBEVENT_DONE_STATUS_ABORTED   (0xF)
+
+#define BLE_HS_CS_PROC_DONE_STATUS_COMPLETED (0x0)
+#define BLE_HS_CS_PROC_DONE_STATUS_PARTIAL   (0x1)
+#define BLE_HS_CS_PROC_DONE_STATUS_ABORTED   (0xF)
+
 int ble_hs_hci_evt_le_cs_rd_rem_supp_cap_complete(uint8_t subevent, const void *data, unsigned int len);
 int ble_hs_hci_evt_le_cs_rd_rem_fae_complete(uint8_t subevent, const void *data, unsigned int len);
 int ble_hs_hci_evt_le_cs_sec_enable_complete(uint8_t subevent, const void *data, unsigned int len);
