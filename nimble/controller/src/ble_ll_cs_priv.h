f2fa5dfb12317ff10ca62f72bbf7f5e6f8875435
Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>
2024-04-16 11:19:58 +0200

    nimble/ll: Add CS Start procedure

--- nimble/controller/src/ble_ll_cs_priv.h
+++ nimble/controller/src/ble_ll_cs_priv.h
@@ -28,7 +28,21 @@
 extern "C" {
 #endif
 
+#define BLE_LL_CS_ROLE_INITIATOR (0)
+#define BLE_LL_CS_ROLE_REFLECTOR (1)
+
 #define BLE_LL_CS_CONFIG_MAX_NUM 4
+/* CS Event interval in units of the number of connection intervals */
+#define BLE_LL_CS_EVENT_INTERVAL_MIN (0x0001)
+#define BLE_LL_CS_EVENT_INTERVAL_MAX (0xFFFF)
+#define BLE_LL_CS_SUBEVENTS_PER_PROCEDURE_MIN (1)
+#define BLE_LL_CS_SUBEVENTS_PER_PROCEDURE_MAX (32)
+/* CS Subevent interval in units of 625usec */
+#define BLE_LL_CS_SUBEVENTS_INTERVAL_MIN (1)
+#define BLE_LL_CS_SUBEVENTS_INTERVAL_MAX (32)
+/* CS Subevent interval in microseconds */
+#define BLE_LL_CS_SUBEVENT_LEN_MIN (1250)
+#define BLE_LL_CS_SUBEVENT_LEN_MAX (4000000)
 
 struct ble_ll_cs_supp_cap {
     uint8_t mode_types;
@@ -69,6 +83,42 @@ struct ble_ll_cs_pref_proc_params {
     uint8_t snr_control_initiator;
     uint8_t snr_control_reflector;
     uint8_t params_ready;
+};
+
+/* Negotiated parameters for CS procedures */
+struct ble_ll_cs_proc_params {
+    /* The number of consecutive CS procedures to invoke */
+    uint16_t max_procedure_count;
+    /* The event counter that will be the anchor point to
+     * the start of the first CS procedure.
+     */
+    uint16_t anchor_conn_event_cntr;
+    /* The offset (usec) from the event counter to the CS subevent anchor point */
+    uint32_t event_offset;
+    uint32_t offset_min;
+    uint32_t offset_max;
+    /* Max CS procedure length in 0.625us units */
+    uint16_t max_procedure_len;
+    /* Max CS subevent length in microseconds */
+    uint32_t subevent_len;
+    uint16_t procedure_interval;
+    uint16_t event_interval;
+    uint16_t subevent_interval;
+    uint8_t subevents_per_event;
+    /* Selected Anntena Configuration ID */
+    uint8_t aci;
+    /* Bit map of preferred peer-ordered antenna elements */
+    uint8_t preferred_peer_antenna;
+    /* Bit map of the remote device’s Tx PHY */
+    uint8_t phy;
+    /* Transmit power delta, in signed dB, to indicate the recommended difference
+     * between the remote device’s power level for the CS tones and RTT packets
+     * and the existing power level for the PHY indicated by the PHY parameter.
+     */
+    uint8_t tx_power_delta;
+    /* SNR Output Index (SOI) for SNR control adjustment */
+    uint8_t tx_snr_i;
+    uint8_t tx_snr_r;
 };
 
 struct ble_ll_cs_config {
@@ -112,6 +162,8 @@ struct ble_ll_cs_config {
     uint8_t t_pm;
     /* CS procedure parameters preferred by our Host */
     struct ble_ll_cs_pref_proc_params pref_proc_params;
+    /* Final negationed CS procedure params */
+    struct ble_ll_cs_proc_params proc_params;
 };
 
 struct ble_ll_cs_sm {
@@ -128,6 +180,9 @@ struct ble_ll_cs_sm {
     uint8_t remote_fae_table[72];
     uint8_t local_fae_table[72];
 
+    struct ble_ll_cs_config *active_config;
+    uint8_t active_config_id;
+
     /* Arguments for ble_ll_cs_config_req_make */
     uint8_t config_req_id;
     uint8_t config_req_action;
@@ -138,6 +193,8 @@ struct ble_ll_cs_sm {
 
     uint8_t measurement_enabled;
 };
+
+int ble_ll_cs_proc_scheduling_start(struct ble_ll_conn_sm *connsm, uint8_t config_id);
 
 #ifdef __cplusplus
 }
