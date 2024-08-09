0b2ef91efe8e137574a8b912ee47a43b469d5fe1
Vipul Rahane <vipul.rahane@juul.com>
2024-08-08 17:18:24 -0700

    Revert "nimble/phy/cmac: Apply preferred RF setting per silicon  variant (TSMC and GF)."
    This reverts commit c514f5b574e425e34448f7843f7cd81dce49343a.

--- nimble/drivers/dialog_cmac/src/ble_rf.c
+++ nimble/drivers/dialog_cmac/src/ble_rf.c
@@ -233,9 +233,6 @@ ble_rf_synth_is_enabled(void)
 static void
 ble_rf_synth_apply_recommended_settings(void)
 {
-    if (mcu_chip_variant_get() == MCU_CHIP_VARIANT_GF) {
-        set_reg32_mask(0x40022034, 0x00000018, 0x0215807B);
-    }
     set_reg32_mask(0x40022048, 0x0000000c, 0x000000d5);
     set_reg32_mask(0x40022050, 0x00000300, 0x00000300);
     set_reg16_mask(0x40022024, 0x0001, 0x0001);
@@ -338,11 +335,7 @@ ble_rf_calibration_1(void)
     set_reg32(0x40020000, 0x0f168820);
     set_reg32_bits(0x40022000, 0x00000001, 0);
     set_reg32_bits(0x4002101c, 0x00001e00, 0);
-    if (mcu_chip_variant_get() == MCU_CHIP_VARIANT_TSMC) {
-        set_reg32_bits(0x4002001c, 0x0000003f, 47);
-    } else {
-        set_reg32_bits(0x4002001c, 0x0000003f, 44);
-    }
+    set_reg32_bits(0x4002001c, 0x0000003f, 47);
     set_reg8(0x40020006, 1);
     set_reg32(0x40020020, 16);
     set_reg32_bits(0x4002003c, 0x00000800, 1);
@@ -543,8 +536,6 @@ ble_rf_init(void)
 {
     static bool done = false;
     uint32_t val;
-
-    assert(mcu_chip_variant_get());
 
     ble_rf_disable();
 
