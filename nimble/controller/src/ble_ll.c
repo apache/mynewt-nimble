dae0e40cffe96bbb2085c3364ea5d796a0b4c1ed
Michal Gorecki <michal.gorecki@codecoup.pl>
2024-09-16 12:39:38 +0200

    nimble/ll: Add PA ADI support
    This adds ADI support for Periodic Advertising
    in controller.

--- nimble/controller/src/ble_ll.c
+++ nimble/controller/src/ble_ll.c
@@ -1935,6 +1935,10 @@ ble_ll_init(void)
     features |= BLE_LL_FEAT_CS_PCT_QUALITY_IND;
 #endif
 
+#if MYNEWT_VAL(BLE_LL_CFG_FEAT_LL_PERIODIC_ADV_ADI_SUPPORT)
+    features |= BLE_LL_FEAT_PERIODIC_ADV_ADI;
+#endif
+
     lldata->ll_supp_features = features;
 
     /* Initialize random number generation */
