d0c943a163cfb26ec02c5a1d8142cd3068d72c0d
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2024-10-10 16:19:37 +0200

    nimble/host: Send GAP pairing complete event only for pairing
    The pairing complete event was sent also for encryption restore
    procedure which is confusing. This makes sure that we only send that
    event in case actual pairing procedure was used.

--- nimble/host/src/ble_sm_priv.h
+++ nimble/host/src/ble_sm_priv.h
@@ -216,6 +216,7 @@ struct ble_sm_dhkey_check {
 #define BLE_SM_PROC_F_AUTHENTICATED         0x08
 #define BLE_SM_PROC_F_SC                    0x10
 #define BLE_SM_PROC_F_BONDING               0x20
+#define BLE_SM_PROC_F_PAIRING               0x40
 
 #define BLE_SM_KE_F_ENC_INFO                0x01
 #define BLE_SM_KE_F_MASTER_ID               0x02
