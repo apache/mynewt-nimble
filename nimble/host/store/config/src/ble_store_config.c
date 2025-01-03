2fcb7812d765adc6f167c2c6a5c56e2bfaf2c6e9
Guy Mishol <guym@ti.com>
2025-01-03 17:09:32 +0200

    nimble/host: Fix a build warning caused by undeclared ARRAY_SIZE macro
    Use of undeclared ARRAY_SIZE macro causes build warning. Add include to
    "os/util.h" in ble_store_config.c file.

--- nimble/host/store/config/src/ble_store_config.c
+++ nimble/host/store/config/src/ble_store_config.c
@@ -24,6 +24,7 @@
 #include "syscfg/syscfg.h"
 #include "host/ble_hs.h"
 #include "base64/base64.h"
+#include "os/util.h"
 #include "store/config/ble_store_config.h"
 #include "ble_store_config_priv.h"
 
