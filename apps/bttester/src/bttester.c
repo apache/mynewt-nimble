3a66f3aa9a11bc6e3797964987c7f80ec153a578
Szymon Janc <szymon.janc@codecoup.pl>
2025-08-13 16:19:39 +0200

    apps/bttester: Improve BTP logging
    Mark BTP trace logs as BTP.

--- apps/bttester/src/bttester.c
+++ apps/bttester/src/bttester.c
@@ -148,10 +148,8 @@ cmd_handler(struct os_event *ev)
 
     len = le16toh(cmd->hdr.len);
     if (MYNEWT_VAL(BTTESTER_BTP_LOG)) {
-        console_printf("[DBG] received %d bytes: %s\n",
-                       sizeof(cmd->hdr) + len,
-                       string_from_bytes(cmd->data,
-                                         sizeof(cmd->hdr) + len));
+        console_printf("[DBG] BTP received %d bytes: %s\n", sizeof(cmd->hdr) + len,
+                       string_from_bytes(cmd->data, sizeof(cmd->hdr) + len));
     }
 
     btp = find_btp_handler(cmd->hdr.service, cmd->hdr.opcode);
@@ -293,11 +291,11 @@ tester_send_with_index(uint8_t service, uint8_t opcode, uint8_t index,
     }
 
     if (MYNEWT_VAL(BTTESTER_BTP_LOG)) {
-        console_printf("[DBG] send %d bytes hdr: %s\n", sizeof(msg),
-                       string_from_bytes((char *) &msg, sizeof(msg)));
+        console_printf("[DBG] BTP send %d bytes hdr: %s\n", sizeof(msg),
+                       string_from_bytes((char *)&msg, sizeof(msg)));
         if (data && len) {
-            console_printf("[DBG] send %d bytes data: %s\n", len,
-                           string_from_bytes((char *) data, len));
+            console_printf("[DBG] BTP send %d bytes data: %s\n", len,
+                           string_from_bytes((char *)data, len));
         }
     }
 }
