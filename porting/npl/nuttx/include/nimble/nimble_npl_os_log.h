apache/mynewt-nimble

commit da4e2f0f12859079f1bde47cbd69f39026c3cc73

porting: Remove dependency on Mynewt logging system
Define NPL interface that can be used by ports to implement logging.

porting/npl/nuttx/include/nimble/nimble_npl_os_log.h
@@ -17,30 +17,20 @@
  * under the License.
  */
 
-#include "os/os.h"
-#include "host/ble_hs.h"
-#include "host/ble_hs_log.h"
+#ifndef _NIMBLE_NPL_OS_LOG_H_
+#define _NIMBLE_NPL_OS_LOG_H_
 
-void
-ble_hs_log_mbuf(const struct os_mbuf *om)
-{
-    uint8_t u8;
-    int i;
+#include <stdarg.h>
 
-    for (i = 0; i < OS_MBUF_PKTLEN(om); i++) {
-        os_mbuf_copydata(om, i, 1, &u8);
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8);
-    }
-}
+/* Example on how to use macro to generate module logging functions */
+#define BLE_NPL_LOG_IMPL(lvl) \
+        static inline void _BLE_NPL_LOG_CAT(BLE_NPL_LOG_MODULE, \
+                _BLE_NPL_LOG_CAT(_, lvl))(const char *fmt, ...)\
+        {                               \
+            va_list args;               \
+            va_start(args, fmt);        \
+            vprintf(fmt, args);          \
+            va_end(args);               \
+        }
 
-void
-ble_hs_log_flat_buf(const void *data, int len)
-{
-    const uint8_t *u8ptr;
-    int i;
-
-    u8ptr = data;
-    for (i = 0; i < len; i++) {
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8ptr[i]);
-    }
-}
+#endif  /* _NIMBLE_NPL_OS_LOG_H_ */

porting/npl/dummy/include/nimble/nimble_npl_os_log.h
@@ -17,30 +17,11 @@
  * under the License.
  */
 
-#include "os/os.h"
-#include "host/ble_hs.h"
-#include "host/ble_hs_log.h"
+#ifndef _NIMBLE_NPL_OS_LOG_H_
+#define _NIMBLE_NPL_OS_LOG_H_
 
-void
-ble_hs_log_mbuf(const struct os_mbuf *om)
-{
-    uint8_t u8;
-    int i;
+#define BLE_NPL_LOG_IMPL(lvl) \
+    static inline void _BLE_NPL_LOG_CAT(BLE_NPL_LOG_MODULE, \
+            _BLE_NPL_LOG_CAT(_, lvl))(const char *fmt, ...) { }
 
-    for (i = 0; i < OS_MBUF_PKTLEN(om); i++) {
-        os_mbuf_copydata(om, i, 1, &u8);
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8);
-    }
-}
-
-void
-ble_hs_log_flat_buf(const void *data, int len)
-{
-    const uint8_t *u8ptr;
-    int i;
-
-    u8ptr = data;
-    for (i = 0; i < len; i++) {
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8ptr[i]);
-    }
-}
+#endif  /* _NIMBLE_NPL_OS_LOG_H_ */

porting/npl/freertos/include/nimble/nimble_npl_os_log.h
@@ -17,30 +17,20 @@
  * under the License.
  */
 
-#include "os/os.h"
-#include "host/ble_hs.h"
-#include "host/ble_hs_log.h"
+#ifndef _NIMBLE_NPL_OS_LOG_H_
+#define _NIMBLE_NPL_OS_LOG_H_
 
-void
-ble_hs_log_mbuf(const struct os_mbuf *om)
-{
-    uint8_t u8;
-    int i;
+#include <stdarg.h>
 
-    for (i = 0; i < OS_MBUF_PKTLEN(om); i++) {
-        os_mbuf_copydata(om, i, 1, &u8);
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8);
-    }
-}
+/* Example on how to use macro to generate module logging functions */
+#define BLE_NPL_LOG_IMPL(lvl) \
+        static inline void _BLE_NPL_LOG_CAT(BLE_NPL_LOG_MODULE, \
+                _BLE_NPL_LOG_CAT(_, lvl))(const char *fmt, ...)\
+        {                               \
+            va_list args;               \
+            va_start(args, fmt);        \
+            vprintf(fmt, args);          \
+            va_end(args);               \
+        }
 
-void
-ble_hs_log_flat_buf(const void *data, int len)
-{
-    const uint8_t *u8ptr;
-    int i;
-
-    u8ptr = data;
-    for (i = 0; i < len; i++) {
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8ptr[i]);
-    }
-}
+#endif  /* _NIMBLE_NPL_OS_LOG_H_ */

porting/npl/linux/include/nimble/nimble_npl_os_log.h
@@ -17,32 +17,21 @@
  * under the License.
  */
 
-#ifndef __LOG_H__
-#define __LOG_H__
+#ifndef _NIMBLE_NPL_OS_LOG_H_
+#define _NIMBLE_NPL_OS_LOG_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <stdarg.h>
+#include <stdio.h>
 
-static inline void
-log_dummy(void *log, ...)
-{
-    (void)log;
-}
+/* Example on how to use macro to generate module logging functions */
+#define BLE_NPL_LOG_IMPL(lvl) \
+        static inline void _BLE_NPL_LOG_CAT(BLE_NPL_LOG_MODULE, \
+                _BLE_NPL_LOG_CAT(_, lvl))(const char *fmt, ...)\
+        {                               \
+            va_list args;               \
+            va_start(args, fmt);        \
+            vprintf(fmt, args);          \
+            va_end(args);               \
+        }
 
-#ifdef MYNEWT
-#define LOG_DEBUG(_log, _mod, ...)      log_dummy(_log, ## __VA_ARGS__)
-#define LOG_INFO(_log, _mod, ...)       log_dummy(_log, ## __VA_ARGS__)
-#define LOG_WARN(_log, _mod, ...)       log_dummy(_log, ## __VA_ARGS__)
-#define LOG_ERROR(_log, _mod, ...)      log_dummy(_log, ## __VA_ARGS__)
-#define LOG_CRITICAL(_log, _mod, ...)   log_dummy(_log, ## __VA_ARGS__)
-#endif
-
-struct log {
-};
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __LOG_H__ */
+#endif  /* _NIMBLE_NPL_OS_LOG_H_ */

porting/npl/mynewt/include/nimble/nimble_npl_os_log.h
@@ -17,30 +17,15 @@
  * under the License.
  */
 
-#include "os/os.h"
-#include "host/ble_hs.h"
-#include "host/ble_hs_log.h"
+#ifndef _NIMBLE_NPL_OS_LOG_H_
+#define _NIMBLE_NPL_OS_LOG_H_
 
-void
-ble_hs_log_mbuf(const struct os_mbuf *om)
-{
-    uint8_t u8;
-    int i;
+#include <modlog/modlog.h>
+#include <log/log.h>
 
-    for (i = 0; i < OS_MBUF_PKTLEN(om); i++) {
-        os_mbuf_copydata(om, i, 1, &u8);
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8);
-    }
-}
+/* Only include the logcfg header if this version of newt can generate it. */
+#if MYNEWT_VAL(NEWT_FEATURE_LOGCFG)
+#include <logcfg/logcfg.h>
+#endif
 
-void
-ble_hs_log_flat_buf(const void *data, int len)
-{
-    const uint8_t *u8ptr;
-    int i;
-
-    u8ptr = data;
-    for (i = 0; i < len; i++) {
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8ptr[i]);
-    }
-}
+#endif  /* _NIMBLE_NPL_OS_LOG_H_ */

porting/npl/riot/include/nimble/nimble_npl_os_log.h
@@ -17,30 +17,20 @@
  * under the License.
  */
 
-#include "os/os.h"
-#include "host/ble_hs.h"
-#include "host/ble_hs_log.h"
+#ifndef _NIMBLE_NPL_OS_LOG_H_
+#define _NIMBLE_NPL_OS_LOG_H_
 
-void
-ble_hs_log_mbuf(const struct os_mbuf *om)
-{
-    uint8_t u8;
-    int i;
+#include <stdarg.h>
 
-    for (i = 0; i < OS_MBUF_PKTLEN(om); i++) {
-        os_mbuf_copydata(om, i, 1, &u8);
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8);
-    }
-}
+/* Example on how to use macro to generate module logging functions */
+#define BLE_NPL_LOG_IMPL(lvl) \
+        static inline void _BLE_NPL_LOG_CAT(BLE_NPL_LOG_MODULE, \
+                _BLE_NPL_LOG_CAT(_, lvl))(const char *fmt, ...)\
+        {                               \
+            va_list args;               \
+            va_start(args, fmt);        \
+            vprintf(fmt, args);          \
+            va_end(args);               \
+        }
 
-void
-ble_hs_log_flat_buf(const void *data, int len)
-{
-    const uint8_t *u8ptr;
-    int i;
-
-    u8ptr = data;
-    for (i = 0; i < len; i++) {
-        BLE_HS_LOG(DEBUG, "0x%02x ", u8ptr[i]);
-    }
-}
+#endif  /* _NIMBLE_NPL_OS_LOG_H_ */

apps/blemesh_shell/src/main.c
@@ -17,8 +17,6 @@
  * under the License.
  */
 
-#define MESH_LOG_MODULE BLE_MESH_LOG
-
 #include <assert.h>
 #include "os/mynewt.h"
 #include "mesh/mesh.h"

nimble/host/include/host/ble_hs_log.h
@@ -20,6 +20,12 @@
 #ifndef H_BLE_HS_LOG_
 #define H_BLE_HS_LOG_
 
+#ifndef BLE_NPL_LOG_MODULE
+#define BLE_NPL_LOG_MODULE BLE_HS_LOG
+#endif
+
+#include <nimble/nimble_npl_log.h>
+
 /**
  * @file ble_hs_log.h
  *
@@ -32,14 +38,6 @@
  * @ingroup bt_host
  * @{
  */
-
-#include "modlog/modlog.h"
-#include "log/log.h"
-
-/* Only include the logcfg header if this version of newt can generate it. */
-#if MYNEWT_VAL(NEWT_FEATURE_LOGCFG)
-#include "logcfg/logcfg.h"
-#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -57,7 +55,7 @@ struct os_mbuf;
  * @param ...           The format string and additional arguments for the log message.
  */
 #define BLE_HS_LOG(lvl, ...) \
-    BLE_HS_LOG_ ## lvl(__VA_ARGS__)
+    BLE_NPL_LOG(lvl, __VA_ARGS__)
 
 /**
  * @brief Macro for logging a Bluetooth address at a specified log level.
@@ -69,9 +67,9 @@ struct os_mbuf;
  *  @param addr         The Bluetooth address to be logged.
  */
 #define BLE_HS_LOG_ADDR(lvl, addr)                      \
-    BLE_HS_LOG_ ## lvl("%02x:%02x:%02x:%02x:%02x:%02x", \
-                       (addr)[5], (addr)[4], (addr)[3], \
-                       (addr)[2], (addr)[1], (addr)[0])
+    BLE_NPL_LOG(lvl, "%02x:%02x:%02x:%02x:%02x:%02x", \
+                (addr)[5], (addr)[4], (addr)[3], \
+                (addr)[2], (addr)[1], (addr)[0])
 
 
 /**

commit 43b2f1d49997bd4d4d2a4a1d578faec0b691f72e

nuttx/nimble_npl_os_log.h: fix log level config from Kconfig
The log messages control was broken since:
  da4e2f0f12859079f1bde47cbd69f39026c3cc73.
All log messages were printed. With this change we can configure the log
level from Kconfig again.

Signed-off-by: raiden00pl <raiden00@railab.me>

porting/npl/nuttx/include/nimble/nimble_npl_os_log.h
@@ -20,18 +20,45 @@
 #ifndef _NIMBLE_NPL_OS_LOG_H_
 #define _NIMBLE_NPL_OS_LOG_H_
 
-#include <stdarg.h>
-#include <stdio.h>
+#include <nuttx/config.h>
+#include <debug.h>
 
-/* Example on how to use macro to generate module logging functions */
-#define BLE_NPL_LOG_IMPL(lvl) \
-        static inline void _BLE_NPL_LOG_CAT(BLE_NPL_LOG_MODULE, \
-                _BLE_NPL_LOG_CAT(_, lvl))(const char *fmt, ...)\
-        {                               \
-            va_list args;               \
-            va_start(args, fmt);        \
-            vprintf(fmt, args);          \
-            va_end(args);               \
-        }
+/* Debug log level configurable from Kconfig */
+
+#ifdef CONFIG_NIMBLE_DEBUG_ERROR
+#define nimble_err _err
+#else
+#define nimble_err _none
+#endif
+
+#ifdef CONFIG_NIMBLE_DEBUG_WARN
+#define nimble_warn _warn
+#else
+#define nimble_warn _none
+#endif
+
+#ifdef CONFIG_NIMBLE_DEBUG_INFO
+#define nimble_info _info
+#else
+#define nimble_info _none
+#endif
+
+#define DFLT_LOG_DEBUG(msg, ...)    nimble_info(msg, ##__VA_ARGS__)
+#define DFLT_LOG_INFO(msg, ...)     nimble_info(msg, ##__VA_ARGS__)
+#define DFLT_LOG_WARN(msg, ...)     nimble_warn(msg, ##__VA_ARGS__)
+#define DFLT_LOG_ERROR(msg, ...)    nimble_err(msg, ##__VA_ARGS__)
+#define DFLT_LOG_CRITICAL(msg, ...) nimble_err(msg, ##__VA_ARGS__)
+
+#define BLE_HS_LOG_DEBUG(msg, ...)    nimble_info(msg, ##__VA_ARGS__)
+#define BLE_HS_LOG_INFO(msg, ...)     nimble_info(msg, ##__VA_ARGS__)
+#define BLE_HS_LOG_WARN(msg, ...)     nimble_warn(msg, ##__VA_ARGS__)
+#define BLE_HS_LOG_ERROR(msg, ...)    nimble_err(msg, ##__VA_ARGS__)
+#define BLE_HS_LOG_CRITICAL(msg, ...) nimble_err(msg, ##__VA_ARGS__)
+
+#define BLE_EATT_LOG_DEBUG(msg, ...)    nimble_info(msg, ##__VA_ARGS__)
+#define BLE_EATT_LOG_INFO(msg, ...)     nimble_info(msg, ##__VA_ARGS__)
+#define BLE_EATT_LOG_WARN(msg, ...)     nimble_warn(msg, ##__VA_ARGS__)
+#define BLE_EATT_LOG_ERROR(msg, ...)    nimble_err(msg, ##__VA_ARGS__)
+#define BLE_EATT_LOG_CRITICAL(msg, ...) nimble_err(msg, ##__VA_ARGS__)
 
 #endif  /* _NIMBLE_NPL_OS_LOG_H_ */