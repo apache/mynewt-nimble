7912f9da524e19030e02cffc3703d2a6f9f04bea
Piotr Narajowski <piotr.narajowski@codecoup.pl>
2025-08-26 11:21:10 +0200

    nimble/host: store client supported features value
    This adds support for storing client supported features for
    clients with a trusted relationship. Fixes Host qualification test
    case GATT/SR/GAS/BV-04-C

--- nimble/host/src/ble_gatts.c
+++ nimble/host/src/ble_gatts.c
@@ -1718,6 +1718,8 @@ int
 ble_gatts_peer_cl_sup_feat_get(uint16_t conn_handle, uint8_t *out_supported_feat, uint8_t len)
 {
     struct ble_hs_conn *conn;
+    struct ble_store_key_cl_sup_feat feat_key;
+    struct ble_store_value_cl_sup_feat feat_val;
     int rc = 0;
 
     if (out_supported_feat == NULL) {
@@ -1735,8 +1737,16 @@ ble_gatts_peer_cl_sup_feat_get(uint16_t conn_handle, uint8_t *out_supported_feat
         len = BLE_GATT_CHR_CLI_SUP_FEAT_SZ;
     }
 
-    memcpy(out_supported_feat, conn->bhc_gatt_svr.peer_cl_sup_feat,
-           sizeof(uint8_t) * len);
+    if (conn->bhc_sec_state.bonded) {
+        feat_key.peer_addr = conn->bhc_peer_addr;
+        feat_key.idx = 0;
+        ble_store_read_peer_cl_sup_feat(&feat_key, &feat_val);
+        memcpy(out_supported_feat, feat_val.peer_cl_sup_feat,
+               sizeof(uint8_t) * len);
+    } else {
+        memcpy(out_supported_feat, conn->bhc_gatt_svr.peer_cl_sup_feat,
+               sizeof(uint8_t) * len);
+    }
 
 done:
     ble_hs_unlock();
@@ -1747,6 +1757,9 @@ int
 ble_gatts_peer_cl_sup_feat_update(uint16_t conn_handle, struct os_mbuf *om)
 {
     struct ble_hs_conn *conn;
+    struct ble_store_value_cl_sup_feat store_feat;
+    struct ble_store_key_cl_sup_feat feat_key;
+    struct ble_store_value_cl_sup_feat feat_val;
     uint8_t feat[BLE_GATT_CHR_CLI_SUP_FEAT_SZ] = {};
     uint16_t len;
     int rc = 0;
@@ -1800,7 +1813,7 @@ ble_gatts_peer_cl_sup_feat_update(uint16_t conn_handle, struct os_mbuf *om)
             store_feat.peer_addr = conn->bhc_peer_addr;
             memcpy(store_feat.peer_cl_sup_feat, feat, BLE_GATT_CHR_CLI_SUP_FEAT_SZ);
             if (ble_store_write_peer_cl_sup_feat(&store_feat)) {
-                /* XXX: How to report an error? */
+                /* XXX: How should this error get reported? */
             }
             rc = 0;
             goto done;
