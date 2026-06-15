7d967aca4a2163d20a9e4e12e6b9f240c2825e1b
Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>
2026-06-15 18:31:05 +0200

    nimble/ll: Add unit tests for CS parameters validation

--- nimble/controller/test/src/ble_ll_test.c
+++ nimble/controller/test/src/ble_ll_test.c
@@ -28,6 +28,7 @@ TEST_SUITE_DECL(ble_ll_csa2_test_suite);
 TEST_SUITE_DECL(ble_ll_isoal_test_suite);
 TEST_SUITE_DECL(ble_ll_iso_test_suite);
 TEST_SUITE_DECL(ble_ll_cs_drbg_test_suite);
+TEST_SUITE_DECL(ble_ll_cs_config_test_suite);
 
 int
 main(int argc, char **argv)
@@ -38,6 +39,7 @@ main(int argc, char **argv)
     ble_ll_isoal_test_suite();
     ble_ll_iso_test_suite();
     ble_ll_cs_drbg_test_suite();
+    ble_ll_cs_config_test_suite();
 
     return tu_any_failed;
 }
